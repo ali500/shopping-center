4ee387abdd5e2157f0b1ead43a7a224fd2873c27
aglgi <aglgi@chmail.ir>
2024-08-19 12:40:24 +0330

    improve table in admin

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,53 +1,102 @@
 <template>
   <div class="container mx-auto">
-    <div class="overflow-x-auto">
-      <table v-if="status == 'success'" class="table bg-base-100">
-        <!-- head -->
-        <thead>
-          <tr>
-            <th></th>
-            <th>Image</th>
-            <th>Title</th>
-            <th>Price</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          <!-- row -->
-          <tr v-for="(product, index) in store.products">
-            <th>{{ index + 1 }}</th>
-            <td>
-              <img :src="product.image" alt="product-image" class="w-24" />
-            </td>
-            <td class="max-w-lg">{{ product.title }}</td>
-            <td>{{ product.price }}</td>
-            <td>
-              <div class="flex gap-2">
-                <NuxtLink
-                  class="btn btn-sm btn-primary w-20"
-                  :to="`/admin/product/${product.id}`"
-                >
-                  Edit
-                </NuxtLink>
-                <button
-                  class="btn btn-sm btn-error w-20"
-                  @click="deleteProductHandler(product.id)"
-                >
-                  Delete
-                </button>
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      <div v-else-if="status == 'pending'">
-        <span class="loading loading-spinner loading-lg"></span>
+    <template v-if="status == 'success'">
+      <div class="overflow-x-auto">
+        <table class="hidden lg:table bg-base-100">
+          <!-- head -->
+          <thead>
+            <tr>
+              <th></th>
+              <th>Image</th>
+              <th>Title</th>
+              <th>Price</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <!-- row -->
+            <tr v-for="(product, index) in store.products">
+              <th>{{ index + 1 }}</th>
+              <td>
+                <img :src="product.image" alt="product-image" class="w-24" />
+              </td>
+              <td class="max-w-lg">{{ product.title }}</td>
+              <td>{{ product.price }}</td>
+              <td>
+                <div class="flex gap-2">
+                  <NuxtLink
+                    class="btn btn-sm btn-primary w-20"
+                    :to="`/admin/product/${product.id}`"
+                  >
+                    Edit
+                  </NuxtLink>
+                  <button
+                    class="btn btn-sm btn-error w-20"
+                    @click="deleteProductHandler(product.id)"
+                  >
+                    Delete
+                  </button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <table class="table lg:hidden table-xs bg-base-100">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Image</th>
+              <th>Title</th>
+              <th>Price</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <!-- row -->
+            <tr v-for="(product, index) in store.products">
+              <th>{{ index + 1 }}</th>
+              <td>
+                <img :src="product.image" alt="product-image" class="w-24" />
+              </td>
+              <td class="max-w-lg">{{ product.title }}</td>
+              <td>{{ product.price }}</td>
+              <td>
+                <div class="flex flex-col gap-2">
+                  <NuxtLink
+                    class="btn btn-sm btn-primary"
+                    :to="`/admin/product/${product.id}`"
+                  >
+                    Edit
+                  </NuxtLink>
+                  <button
+                    class="btn btn-sm btn-error"
+                    @click="deleteProductHandler(product.id)"
+                  >
+                    Delete
+                  </button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>company</th>
+              <th>location</th>
+            </tr>
+          </tfoot>
+        </table>
       </div>
-      <div v-else>products not found</div>
-      <Toast v-show="toastValues.isShow" :status="toastValues.status">
-        {{ toastValues.message }}
-      </Toast>
+    </template>
+    <div v-else-if="status == 'pending'">
+      <span class="loading loading-spinner loading-lg"></span>
     </div>
+    <div v-else>products not found</div>
+    <Toast v-show="toastValues.isShow" :status="toastValues.status">
+      {{ toastValues.message }}
+    </Toast>
   </div>
 </template>
 
